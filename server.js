1f5e4af5d2ce780131ef060f62932133888ae12d
kevinpaulJose <bkevin1999@gmail.com>
2023-04-03 23:27:51 +0530

    Allow all cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dbo = require("./db/conn");
 const PORT = process.env.PORT || 5000;
 const app = express();
 const corsOptions = {
-  origin: "https://careall.co.in/",
+  origin: "*",
 };
 app.use(cors(corsOptions));
 
